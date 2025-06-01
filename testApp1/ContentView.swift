f62b058b7239bc4a0065fe5fb834a6352f4ac344
Jacky11 <2116403107@qq.com>
2025-06-01 17:33:27 -0400

    Initial Commit

---
+++ testApp1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  testApp1
+//
+//  Created by 虞源正Jack on 2025/6/1.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ testApp1/testApp1App.swift
@@ -0,0 +1,17 @@
+//
+//  testApp1App.swift
+//  testApp1
+//
+//  Created by 虞源正Jack on 2025/6/1.
+//
+
+import SwiftUI
+
+@main
+struct testApp1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
